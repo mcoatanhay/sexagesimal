4da097908b9bf2bb9487bfeaa96bc49c04300ad0
mcoatanhay@gmail.com <mcoatanhay@gmail.com>
2020-02-21 23:00:10 +0100

    documentation

---
+++ sexag.py
@@ -0,0 +1,60 @@
+﻿#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Fichier: sexag.py
+# Auteur: Marc COATANHAY
+
+"""
+    Module python pour effectuer les calculs au format sexagesimal.
+"""
+
+# Import des modules
+
+# Définitions constantes et variables globales
+signes = {-1: "-", 1: "+"}
+
+def todec(terme):
+    """transforme un terme sexagésimal en décimal"""
+    return (terme.u + terme.m/60 + terme.s/3600)*terme.sgn
+
+def tosexag(terme):
+    """transforme un terme au format sexagésimal"""
+    sgn = int(terme/abs(terme))
+    terme *= sgn
+    u = int(terme)
+    m = int((terme - u)*60)
+    s = (terme - u - m/60)*3600
+    return Sexag(sgn, u, m, s)
+
+class Sexag:
+    """ Class pour traiter les nombres sexagésimaux"""
+    def __add__(self, terme2):
+        """Additionne deux termes Sexag."""
+        return tosexag(todec(self) + todec(terme2))
+
+    def __format__(self,chaine):
+        return self.__repr__()
+
+    def __init__(self,sgn=1, u=0 , m=0 , s=0):
+        """Initialise un élément Sexag."""
+        self.sgn = sgn
+        self.u = u
+        self.m = m
+        self.s = s
+
+    def __lt__(self, terme):
+        """Compare un élément Sexag avec un nombre"""
+        return(todec(self) < terme)
+
+    def __mul__(self, terme):
+        """Multiplie un terme Sexag par un nombre."""
+        return tosexag(todec(self)*terme)
+
+    def __repr__(self):
+        """Affichage d'un terme Sexag."""
+        chaine = "{}{}:{}:{}".format(signes[self.sgn], self.u, self.m, self.s)
+        return chaine
+
+
+
+
+
---
+++ test_sexag.py
@@ -0,0 +1,32 @@
+﻿#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Fichier: test_sexag.py
+# Auteur: Marc COATANHAY
+
+"""
+    Tests pour la class Sexag.
+"""
+
+# Import des modules
+from sexag import *
+import unittest
+
+# Définitions constantes et variables globales
+a = Sexag(1, 1, 30, 36)
+
+# Définitions fonctions et classes
+class SexagTest(unittest.TestCase):
+    def test_dec(self):
+        self.assertEqual(todec(a), 1.51)
+
+    def test_add(self):
+        self.assertEqual(todec(a + a), 3.02)
+
+    def test_lt(self):
+        self.assertTrue(a < 1.511)
+
+    def test_mul(self):
+        self.assertEqual(todec(a*10), 15.1)
+
+if (__name__ == "__main__"):
+    unittest.main()
